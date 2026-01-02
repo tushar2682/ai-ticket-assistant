019e634969709a9cb35323801dd6ea71c2531691
ayush41 <anonymousking41681@gmail.com>
2026-01-02 22:07:45 +0530

    fixed: loader

--- ai-ticket-frontend/tailwind.config.js
+++ ai-ticket-frontend/tailwind.config.js
@@ -1,13 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
-})
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from 'tailwindcss/vite' 
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//   ],
+// })
 
 
 
